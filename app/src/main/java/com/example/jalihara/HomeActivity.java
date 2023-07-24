f68563789cefda900e97abd80479738532375c53
michaelbaptis <michaelbaptis@gmail.com>
2023-07-25 00:08:58 +0700

    check merge data

--- app/src/main/java/com/example/jalihara/HomeActivity.java
+++ app/src/main/java/com/example/jalihara/HomeActivity.java
@@ -1,32 +1,42 @@
 package com.example.jalihara;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
 import android.widget.TextView;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
     private TextView tvUsername;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
         TextView tvUsername = findViewById(R.id.tvUsername);
-
         // Mengambil data username yang dikirimkan dari LoginActivity
         String username = getIntent().getStringExtra("username");
-
         if (username != null && !username.isEmpty()) {
             // Menampilkan username di TextView
             tvUsername.setText("Welcome, " + username + "!");
         }
+
+        ImageSlider imageSlider = findViewById(R.id.imageSlider);
+        ArrayList<SlideModel> slideModels = new ArrayList<>();
+
+        slideModels.add(new SlideModel(R.drawable.image1, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.image2, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.image3, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.image4, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.image5, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.image6, ScaleTypes.FIT));
+
+        imageSlider.setImageList(slideModels, ScaleTypes.FIT);
     }
 
 }
